620d042789f51d1c784ae0f15ba0345ca4563c37
cld378632668 <378632668@qq.com>
2017-12-28 13:01:46 +0800

    update

--- push.bat
+++ push.bat
@@ -1,3 +1,4 @@
+cd /d G:\[important]MyCode\[Important]JavaApiUtilOrExample
 echo abcdefg > crontab.txt
 git add .
 git commit -m "update"
@@ -6,3 +7,4 @@ git rm crontab.txt
 git add .
 git commit -m "update"
 git push
+
